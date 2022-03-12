ba4c5307a377b595af44a5fb091c4534c0773711
DaatKruby <marcos.garcia@potros.itson.edu.mx>
2022-03-12 15:50:46 -0600

    hardcoded coffe-info card added

---
+++ components/card.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import cls from 'classnames'
+
+import styles from '../styles/Card.module.css';
+
+export default function Card(props) {
+    return (
+        <Link href={props.href}>
+            <a className={styles.cardLink}>
+                <div className={cls("glass", styles.container)}>
+                    <div className={styles.cardHeaderWrapper}>
+                        <h2 className={styles.cardHeader}>
+                            {props.name}
+                        </h2>
+                    </div>
+                    <div className={styles.cardImageWrapper}>
+                        <Image
+                            src={props.imgUrl}
+                            className={styles.cardImage}
+                            width={260}
+                            height={160} />
+                    </div>
+                </div>
+            </a>
+        </Link>
+    )
+}
