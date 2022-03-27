705811dffcc10b3be5e37b2ed0c78dc4c5154585
DaatKruby <marcos.garcia@potros.itson.edu.mx>
2022-03-27 10:58:14 -0700

    fixed typos

--- pages/api/coffeeStores.js
+++ pages/api/coffeeStores.js
@@ -9,7 +9,12 @@ export default async function handler(req, res) {
     let config = {
       method: 'get',
       url:
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latLong}&radius=15000&type=cafe&keyword=cafe&key=${apikey}`,
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?
+        location=${latLong}&
+        radius=15000&
+        type=cafe&
+        keyword=cafe&
+        key=AIzaSyCjJGTpFj9YfUrRsX2mDlniMfCEf9oF_9E`,
       headers: {}
     };
 
